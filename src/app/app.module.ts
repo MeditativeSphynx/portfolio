86a91b52ebf18d653c2b8d7ca4c1c8cbe2a3b80f
Daniel <meditativesphynx@gmail.com>
2023-04-06 08:50:29 -0400

    add resume component(s)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResumeComponent } from './pages/resume/resume.component';
+import { SkillsComponent } from './pages/resume/skills/skills.component';
+import { JobHistoryComponent } from './pages/resume/job-history/job-history.component';
+import { JobComponent } from './pages/resume/job-history/job/job.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResumeComponent,
+    SkillsComponent,
+    JobHistoryComponent,
+    JobComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/resume/job-history/job-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-job-history',
+  templateUrl: './job-history.component.html',
+  styleUrls: ['./job-history.component.sass']
+})
+export class JobHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
