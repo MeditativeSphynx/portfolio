2cd1bdd1807551d0b0dc468d89545a40af86b7dc
Daniel <meditativesphynx@gmail.com>
2023-11-27 09:50:00 -0500

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ResumeComponent } from 'src/app/pages/resume/resume.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ChangelogComponent } from './pages/changelog/changelog.component';
-import { InterestsComponent } from './pages/interests/interests.component';
+// import { InterestsComponent } from './pages/interests/interests.component';
 
 const routes: Routes = [
   { path: 'resume', component: ResumeComponent },
